c2806182786147de617f254d9037e89dcdd503fe
Govind Jha <govind@worktabe.sg>
2021-07-07 17:56:36 +0530

    Created UI switch gear selection screen

--- lib/SwitchGearSelection.dart
+++ lib/SwitchGearSelection.dart
@@ -11,6 +11,8 @@ class SwitchGearSelection extends StatefulWidget {
 }
 
 class _SwitchGearSelection extends State<SwitchGearSelection> {
+  String selectedOption = "KW";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,69 @@ class _SwitchGearSelection extends State<SwitchGearSelection> {
         ),
         body: Container(
           color: Colors.white,
-          padding: EdgeInsets.all(16.0),
+          padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
+          child: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Value',
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'From : ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    new Radio(
+                      value: 'kw',
+                      groupValue: selectedOption,
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedOption = value!;
+                        });
+                      },
+                    ),
+                    new Text(
+                      'KW',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                    new Radio(
+                      value: 'hp',
+                      groupValue: selectedOption,
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedOption = value!;
+                        });
+                      },
+                    ),
+                    new Text(
+                      'HP',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text('Convert'),
+                  onPressed: () {
+                    print('Converted');
+                  },
+                ),
+              ),
+            ],
+          ),
         ));
   }
 }
